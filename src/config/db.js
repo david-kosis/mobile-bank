359097fc5cffd3954c0f3867c6a14ed0993a6659
david-kosis <davidkosis64@gmail.com>
2026-09-12 15:50:34 +0100

    Remove duplicate backend source

--- src/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-export async function connectDB() {
-  const uri = process.env.MONGO_URI;
-  if (!uri) throw new Error('MONGO_URI is not configured');
-  await mongoose.connect(uri);
-  console.log('MongoDB connected');
-}
